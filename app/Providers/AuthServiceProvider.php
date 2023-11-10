8878b81284835c3bb8891eebcb3c307c4976b03b
Briliantoro <gakyasa@gmail.com>
2023-11-10 14:01:34 +0700

    Authorize, User Role, Update Profile, Password, List User, Pagination, Search

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,9 +4,16 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
+    public static $permission = [
+        'dashboard' => ['superadmin', 'admin'],
+        'user-index' => ['admin'],
+        'payment-update' => ['superadmin'],
+    ];
     /**
      * The model to policy mappings for the application.
      *
@@ -23,8 +30,17 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
 
-        //
+        foreach(self::$permission as $feature => $roles) {
+           Gate::define($feature, function (User $user) use ($roles) {
+            if (in_array($user->role, ['superadmin'])) {
+                return true;
+            }
+        });  
+       
+        
+    }
+
+       
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,11 +13,24 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create();
+        User::factory(20)->create();
         User::create([
             'name'=>'vivienvarian',
             'email'=>'supervivien@gmail.com',
             'email_verified_at' => now(),
+            'role' => 'admin',
+            'phone' => '6285713407586',
+            'bio' => 'flutter dev',
+            'password' => Hash::make('123456'), 
+        ]);
+
+        User::create([
+            'name'=>'kang admin',
+            'email'=>'superadmin@gmail.com',
+            'email_verified_at' => now(),
+            'role' => 'superadmin',
+            'phone' => '6285713407587',
+            'bio' => 'laravel dev',
             'password' => Hash::make('123456'), 
         ]);
     }
