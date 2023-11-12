80e68ad841ed402d2c544c77c6820b43e7aaf1a7
Briliantoro <gakyasa@gmail.com>
2023-11-12 19:51:53 +0700

    pertemuan 4 dan 5

---
+++ database/migrations/2023_11_12_062338_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->double('price');
+            $table->string('image_url');
+            $table->foreignIdFor(Category::class);
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
